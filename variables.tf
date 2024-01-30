1ec9e2de2b244f132d616133a70cfded6efbfacd
Commander_Cobra <gregawscsa@gmail.com>
2024-01-29 20:37:38 -0500

    fixing gcp before hcp vault issued

---
+++ variables.tf
@@ -0,0 +1,8 @@
+# variables.tf
+
+variable "google_credentials_path" {
+  description = "Path to the Google Cloud credentials JSON file"
+  type        = string
+  default     = "path/to/your/credentials.json"
+}
+
