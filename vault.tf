0d6a1aea968a25c1a43ad495d3fff26b791baec1
Commander_Cobra <gregawscsa@gmail.com>
2024-01-29 21:00:39 -0500

    adding aws

--- vault.tf
+++ vault.tf
@@ -1,8 +1,20 @@
-# Use the vault data source in your Terraform configuration to retrieve the Google Cloud credentials
+# Use the vault data source in your Terraform configuration to retrieve Cloud credentials. This is static see dynamic credentials for best practice 
 
 data "vault_generic_secret" "google_credentials" {
   path = "secret/data/google_credentials"
 }
+
+data "vault_generic_secret" "aws_credentials" {
+  path = "secret/data/aws-credentials"
+}
+
+provider "aws" {
+  access_key = data.vault_generic_secret.aws_credentials.data["data"]["access_key"]
+  secret_key = data.vault_generic_secret.aws_credentials.data["data"]["secret_key"]
+  region     = data.vault_generic_secret.aws_credentials.data["data"]["region"]
+}
+
+
 
 provider "google" {
   alias       = "vault"
