9e9b757e1349b1e5669360882298442932f59a7c
Commander_Cobra <gregawscsa@gmail.com>
2024-01-29 21:13:39 -0500

    added all 3 variable and data sources

--- variables.tf
+++ variables.tf
@@ -1,5 +1,19 @@
 # variables.tf
 
+variable "aws_credentials_path" {
+  description = "Path to the AWS credentials JSON file"
+  type        = string
+  default     = "path/to/your/credentials.json"
+}
+
+
+variable "Azure_credentials_path" {
+  description = "Path to the Google Cloud credentials JSON file"
+  type        = string
+  default     = "path/to/your/credentials.json"
+}
+
+
 variable "google_credentials_path" {
   description = "Path to the Google Cloud credentials JSON file"
   type        = string
--- vault.tf
+++ vault.tf
@@ -1,5 +1,11 @@
 # Use the vault data source in your Terraform configuration to retrieve Cloud credentials. This is static see dynamic credentials for best practice 
 
+
+data "vault_generic_secret" "azure_credentials" {
+  path = "secret/data/azure_credentials"
+}
+
+
 data "vault_generic_secret" "google_credentials" {
   path = "secret/data/google_credentials"
 }
@@ -15,7 +21,14 @@ provider "aws" {
   region     = data.vault_generic_secret.aws_credentials.data["data"]["region"]
 }
 
-
+provider "azurerm" {
+  alias                    = "vault"
+  subscription_id          = data.vault_generic_secret.azure_credentials.data["data"]["subscription_id"]
+  client_id                = data.vault_generic_secret.azure_credentials.data["data"]["client_id"]
+  client_secret            = data.vault_generic_secret.azure_credentials.data["data"]["client_secret"]
+  tenant_id                = data.vault_generic_secret.azure_credentials.data["data"]["tenant_id"]
+  features                 = {}  # Use an empty block to enable all features
+}
 
 provider "google" {
   alias       = "vault"
